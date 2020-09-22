ff8cafb30c2e4bb64a3ded45087acaa004ea7ffe
yarimati <nikimihiki@gmail.com>
2020-09-22 17:27:20 +0300

    Добавьте файлы проекта.

---
+++ WeatherBot/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WeatherBot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
